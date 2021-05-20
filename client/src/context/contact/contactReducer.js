627e28eaeb99c8e51095503b30454951be54321c
Luciano Pastine <LP1@MacBook-Pro-de-Luciano.local>
2021-05-20 17:30:46 -0300

    Add Contact

--- client/src/context/contact/contactReducer.js
+++ client/src/context/contact/contactReducer.js
@@ -5,6 +5,7 @@ import {
     CLEAR_CURRENT,
     UPDATE_CONTACT,
     FILTER_CONTACTS,
+    CONTACT_ERROR,
     CLEAR_FILTER
 } from '../types';
 
@@ -48,6 +49,11 @@ export default (state, action) => {
                 ...state,
                 filtered: null
             };
+        case CONTACT_ERROR:
+            return {
+                ...state,
+                error: action.payload,
+            }
         default:
             return state;
     }
