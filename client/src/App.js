872d5618ac7fea7443af4e88008f53e4a4c1fe3f
Luciano Pastine <LP1@MacBook-Pro-de-Luciano.local>
2021-05-20 17:11:51 -0300

    PrivateRoute Component

--- client/src/App.js
+++ client/src/App.js
@@ -8,6 +8,7 @@ import About from './components/pages/About';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
 import Alerts from './components/layout/Alerts';
+import PrivateRoute from './components/routing/PrivateRoute';
 
 import ContactState from './context/contact/ContactState';
 
@@ -33,7 +34,7 @@ const App = () => {
               <div className="container">
                 <Alerts />
                 <Switch>
-                  <Route exact path='/' component={Home} />
+                  <PrivateRoute exact path='/' component={Home} />
                   <Route exact path='/about' component={About} />
                   {/* 9(b) - Create Route for the Register Component */}
                   <Route exact path='/register' component={Register} />
